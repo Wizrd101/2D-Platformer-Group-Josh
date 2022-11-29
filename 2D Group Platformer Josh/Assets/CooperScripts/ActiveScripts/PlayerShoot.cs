afbe5112c52edc1a3e3bd44ce8b3f97972283932
AradFox <115174667+AradFox@users.noreply.github.com>
2022-11-29 09:43:35 -0800

    added fire animation when gun fired

--- 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlayerShoot.cs
+++ 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlayerShoot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerShoot : MonoBehaviour
 {
     public GameObject bullet;
@@ -10,14 +11,27 @@ public class PlayerShoot : MonoBehaviour
     public AudioClip shootSound;
     float timer = 0.0f;
     public float shootDelay;
+    public static bool grounded = false;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+
         timer += Time.deltaTime;
         if (Time.timeScale == 1)
         {
             if (Input.GetButtonDown("Fire1") && timer >= shootDelay && PlatformerMovement.grounded == true)
             {
+                animator.SetTrigger("Active");
                 GameObject bulletSpawn = Instantiate(bullet, transform.position, Quaternion.identity);
                 Vector3 mousePosition = Input.mousePosition;
                 mousePosition.z = -Camera.main.transform.position.z;
@@ -29,6 +43,16 @@ public class PlayerShoot : MonoBehaviour
                 GetComponent<AudioSource>().PlayOneShot(shootSound);
                 timer = 0;
             }
+        }
+        animator.SetFloat("xInput", xInput);
+        animator.SetBool("grounded", grounded);
+        if (xInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (xInput > 0)
+        {
+            spriteRenderer.flipX = false;
         }
     }
 }
--- 2D Group Platformer Josh/Assets/Player/LookAtMouse.cs
+++ 2D Group Platformer Josh/Assets/Player/LookAtMouse.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class LookAtMouse : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    SpriteRenderer playerSpriteRenderer;
 
     // Use this for initialization
     void Start()
     {
-
+        playerSpriteRenderer = GameObject.Find("Player (1)").GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -16,5 +19,15 @@ public class LookAtMouse : MonoBehaviour
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //to set flipped gun back to normal
+        if (playerSpriteRenderer.flipX)
+        {
+            spriteRenderer.flipX = true;
+
+        }
+        else if (!playerSpriteRenderer.flipX)
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
