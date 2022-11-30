0de362279ddb1595219bf18a821cdcab54db52a1
AradFox <115174667+AradFox@users.noreply.github.com>
2022-11-30 10:28:52 -0800

    Added more gun stuff

--- 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlayerShoot.cs
+++ 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlayerShoot.cs
@@ -1,11 +1,14 @@
  using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class PlayerShoot : MonoBehaviour
 {
+    public PlayerShoot playerShoot;
     public GameObject bullet;
+    public int currentCylinder, maxMagSize = 6, currentReserves, maxReserveSize = 90;
     public float speed;
     public float bulletLifeTime = 1.5f;
     public AudioClip shootSound;
@@ -19,6 +22,7 @@ public class PlayerShoot : MonoBehaviour
     {
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -29,7 +33,7 @@ public class PlayerShoot : MonoBehaviour
         timer += Time.deltaTime;
         if (Time.timeScale == 1)
         {
-            if (Input.GetButtonDown("Fire1") && timer >= shootDelay && PlatformerMovement.grounded == true)
+            if (Input.GetButtonDown("Fire1") && timer >= shootDelay && PlatformerMovement.grounded == true && currentCylinder > 0)
             {
                 animator.SetTrigger("Active");
                 GameObject bulletSpawn = Instantiate(bullet, transform.position, Quaternion.identity);
@@ -40,19 +44,44 @@ public class PlayerShoot : MonoBehaviour
                 shootDirection.Normalize();
                 bulletSpawn.GetComponent<Rigidbody2D>().velocity = shootDirection * speed;
                 Destroy(bulletSpawn, bulletLifeTime);
+                currentCylinder--;
                 GetComponent<AudioSource>().PlayOneShot(shootSound);
                 timer = 0;
             }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                playerShoot.Reload();
+            }
+
+            animator.SetFloat("xInput", xInput);
+            animator.SetBool("grounded", grounded);
+            if (xInput < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (xInput > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+           
         }
-        animator.SetFloat("xInput", xInput);
-        animator.SetBool("grounded", grounded);
-        if (xInput < 0)
+    }
+
+    public void Reload()
+    {
+        int reloadAmount = maxMagSize - currentCylinder;
+        reloadAmount = (currentReserves - reloadAmount) >= 0 ? reloadAmount : currentReserves;
+        currentCylinder += reloadAmount;
+        currentReserves -= reloadAmount;
+    }
+
+    public void AddRounds(int roundAmmount)
+    {
+        currentReserves += roundAmmount;
+        if(currentReserves > maxMagSize)
         {
-            spriteRenderer.flipX = true;
-        }
-        else if (xInput > 0)
-        {
-            spriteRenderer.flipX = false;
+            currentReserves = maxMagSize;
         }
     }
 }
