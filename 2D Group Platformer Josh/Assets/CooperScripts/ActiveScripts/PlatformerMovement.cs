14ed07a3b02c0d8932632bf64f36b457f5dca34c
Cooper Carey <65479428+Jokopoking@users.noreply.github.com>
2022-12-14 09:39:35 -0800

    movement change

--- 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlatformerMovement.cs
+++ 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/PlatformerMovement.cs
@@ -35,49 +35,52 @@ public class PlatformerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
+        if (Health.isDead == false)
+        {
+            float xInput = Input.GetAxis("Horizontal");
 
-        Vector2 velocity = rb.velocity;
-        if (knockbackCounter <= 0)
-        {
-            velocity.x = xInput * moveSpeed;
-            rb.velocity = velocity;
-        }
-        else
-        {
-            if (knockFromRight == true)
+            Vector2 velocity = rb.velocity;
+            if (knockbackCounter <= 0)
             {
-                //velocity.x = xInput * moveSpeed;
-                rb.velocity = new Vector2(-knockbackAmount, knockbackAmount);
+                velocity.x = xInput * moveSpeed;
+                rb.velocity = velocity;
             }
-            if (knockFromRight == false) 
+            else
             {
-                //velocity.x = xInput * moveSpeed;
-                rb.velocity = new Vector2(-knockbackAmount, knockbackAmount);
+                if (knockFromRight == true)
+                {
+                    //velocity.x = xInput * moveSpeed;
+                    rb.velocity = new Vector2(-knockbackAmount, knockbackAmount);
+                }
+                if (knockFromRight == false)
+                {
+                    //velocity.x = xInput * moveSpeed;
+                    rb.velocity = new Vector2(-knockbackAmount, knockbackAmount);
+                }
+                knockbackCounter -= Time.deltaTime;
             }
-            knockbackCounter -= Time.deltaTime;
-        }
 
-        if (Input.GetButtonDown("Jump") && grounded == true)
-        {
-            animator.SetTrigger("Jump");
-            rb.AddForce(new Vector2(0, 100 * jumpSpeed));
-            grounded = false;
-            
-        }
-        if (rb.velocity.y < -0.1f && !grounded)
-        {
-            animator.SetTrigger("Fall");
-        }
-        animator.SetFloat("xInput", xInput);
-        animator.SetBool("grounded", grounded);
-        if (xInput < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else if(xInput > 0)
-        {
-            spriteRenderer.flipX = false;
+            if (Input.GetButtonDown("Jump") && grounded == true)
+            {
+                animator.SetTrigger("Jump");
+                rb.AddForce(new Vector2(0, 100 * jumpSpeed));
+                grounded = false;
+
+            }
+            if (rb.velocity.y < -0.1f && !grounded)
+            {
+                animator.SetTrigger("Fall");
+            }
+            animator.SetFloat("xInput", xInput);
+            animator.SetBool("grounded", grounded);
+            if (xInput < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (xInput > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
         }
     }
         private void OnTriggerEnter2D(Collider2D collision)
@@ -117,4 +120,5 @@ public class PlatformerMovement : MonoBehaviour
         }
     }
 }
+
 
