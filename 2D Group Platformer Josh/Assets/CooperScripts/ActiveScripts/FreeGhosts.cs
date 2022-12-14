645a4e03a622263a7eeb2f6b9edf4d27f1eb256a
Cooper Carey <65479428+Jokopoking@users.noreply.github.com>
2022-12-14 08:40:56 -0800

    cage prefabs

--- 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/FreeGhosts.cs
+++ 2D Group Platformer Josh/Assets/CooperScripts/ActiveScripts/FreeGhosts.cs
@@ -15,7 +15,7 @@ public class FreeGhosts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinText.text = $"Ghosts Freed: {ghostCount}";
     }
 
     // Update is called once per frame
