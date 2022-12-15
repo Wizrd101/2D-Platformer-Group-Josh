4680f35c10d74aac76b4e8c315bee13d1cbb6f6c
Wizrd101 <simonhenderson100@gmail.com>
2022-12-15 08:48:32 -0800

    Defeated Male leaves
    I can't get the random portals to work properly, so I set up some random ones that might or might not be temporary depending on how much time I have.
    Ima work on the UI and Parallax art now

--- 2D Group Platformer Josh/Assets/PortalTeleport.cs
+++ 2D Group Platformer Josh/Assets/PortalTeleport.cs
@@ -10,7 +10,6 @@ public class PortalTeleport : MonoBehaviour
         if (other.tag == "Player")
         {
             other.transform.position = new Vector2(teleportPoint.transform.position.x, teleportPoint.transform.position.y);
-            
         }
     }
 }
